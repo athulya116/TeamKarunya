71288ce5cabc98738f04a5cc93088cd1a5615fde
athulya116 <athulyaathu9562@gmail.com>
2026-03-29 20:54:44 +0530

    changes in home page view

--- teamKarunya/teamK/views.py
+++ teamKarunya/teamK/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+﻿from django.shortcuts import render
 from .models import HomeSliderImage, HomePageGallery,ServicesPage, GalleryImage, SiteLogo
 
 # Create your views here.
--- teamKarunya/teamKarunya/settings.py
+++ teamKarunya/teamKarunya/settings.py
@@ -29,8 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "fallback-insecure-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
 
 
 # ALLOWED_HOSTS = ["teamkarunya.onrender.com", "localhost","teamkarunya.com"]
